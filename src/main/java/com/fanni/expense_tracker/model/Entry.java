ceca8bf40f35e2d10c9d32ebc24745fa91feed80
fanniNagy <nagy.fanni.5@gmail.com>
2021-01-05 15:34:22 +0100

    refactor to entity

--- src/main/java/com/fanni/expense_tracker/model/Entry.java
+++ src/main/java/com/fanni/expense_tracker/model/Entry.java
@@ -1,16 +1,27 @@
 package com.fanni.expense_tracker.model;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.time.LocalDate;
 
 @Data
+@NoArgsConstructor
 @Builder
+@Entity
+@AllArgsConstructor
 public class Entry {
 
-    private final Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private int price;
+
     private String name;
+
     @Builder.Default
     private LocalDate date = LocalDate.now();
 
