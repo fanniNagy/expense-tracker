f85df9f5a875354fb9ceb57e9ed59b2cd38235e9
fanniNagy <nagy.fanni.5@gmail.com>
2021-01-06 16:49:30 +0100

    add tests for EntryService

---
+++ src/test/java/com/fanni/expense_tracker/service/EntryServiceTest.java
@@ -0,0 +1,61 @@
+package com.fanni.expense_tracker.service;
+
+import com.fanni.expense_tracker.model.Entry;
+import com.fanni.expense_tracker.repository.EntryRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+public class EntryServiceTest {
+
+    private final EntryService service;
+    private final EntryRepository repository;
+
+    @Autowired
+    public EntryServiceTest(EntryService service, EntryRepository repository) {
+        this.service = service;
+        this.repository = repository;
+    }
+
+    @Test
+    void givenDatesAreCorrect_WhenRandomDateGenerates_ThenReturnsCorrectDate() {
+        LocalDate from = LocalDate.of(2020, 1, 1);
+        LocalDate to = LocalDate.of(2021, 1, 1);
+        LocalDate generated = service.generateRandomEntry().getDate();
+        assertTrue(generated.isAfter(from) && generated.isBefore(to));
+    }
+
+    @Test
+    void givenEntryGenerated_WhenBuilt_ThenNameNotNull() {
+        Entry generatedEntry = service.generateRandomEntry();
+        assertNotNull(generatedEntry.getName());
+    }
+
+    @Test
+    void givenWhenRandomEntryCreated_ThenEntrySavedToRepository() {
+        Entry createdEntry = service.createRandomEntry();
+        Optional<Entry> savedEntry = repository.findById(createdEntry.getId());
+        assertEquals(createdEntry, savedEntry.orElseThrow(() -> new RuntimeException("Entry is not present")));
+    }
+
+    @Test
+    void givenEntriesAreSaved_WhenEntriesAreQueried_ThenAllGetsReturned() {
+        service.clearRepository();
+        List<Entry> entries = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            Entry entry = service.createRandomEntry();
+            entries.add(entry);
+        }
+        List<Entry> repositoryEntries = new ArrayList<>(service.getAllEntries());
+        assertIterableEquals(entries, repositoryEntries);
+    }
+
+}
