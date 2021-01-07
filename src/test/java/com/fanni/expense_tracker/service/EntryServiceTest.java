9329928425a149ddc284fe2ba1f8f21c18caf756
fanniNagy <nagy.fanni.5@gmail.com>
2021-01-07 11:58:39 +0100

    add tests for finding entries by dates/price between range

--- src/test/java/com/fanni/expense_tracker/service/EntryServiceTest.java
+++ src/test/java/com/fanni/expense_tracker/service/EntryServiceTest.java
@@ -2,14 +2,17 @@ package com.fanni.expense_tracker.service;
 
 import com.fanni.expense_tracker.model.Entry;
 import com.fanni.expense_tracker.repository.EntryRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Repeat;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.IntStream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,6 +26,11 @@ public class EntryServiceTest {
     public EntryServiceTest(EntryService service, EntryRepository repository) {
         this.service = service;
         this.repository = repository;
+    }
+
+    @BeforeEach
+    void clearRepository(){
+        service.clearRepository();
     }
 
     @Test
@@ -48,7 +56,6 @@ public class EntryServiceTest {
 
     @Test
     void givenEntriesAreSaved_WhenEntriesAreQueried_ThenAllGetsReturned() {
-        service.clearRepository();
         List<Entry> entries = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             Entry entry = service.createRandomEntry();
@@ -57,5 +64,33 @@ public class EntryServiceTest {
         List<Entry> repositoryEntries = new ArrayList<>(service.getAllEntries());
         assertIterableEquals(entries, repositoryEntries);
     }
+
+    @Test
+    void givenThereAreNoMatchingEntries_WhenEntriesAreQueriedByDatesBetween_ThenNoNullGetsReturned(){
+        LocalDate from = LocalDate.of(2020, 1, 1);
+        LocalDate to = LocalDate.of(2021, 1, 1);
+        assertNotNull(service.findEntriesByDateBetween(from, to));
+    }
+
+    @Test
+    void givenEntriesAreSaved_WhenEntriesAreQueriedByDatesBetween_ThenAllGetsReturned(){
+        IntStream.range(0, 5).forEach(i -> service.createRandomEntry());
+        LocalDate from = LocalDate.of(2020, 1, 1);
+        LocalDate to = LocalDate.of(2021, 1, 1);
+        assertEquals(5, service.findEntriesByDateBetween(from, to).size());
+    }
+
+    @Test
+    void givenThereAreNoMatchingEntries_WhenEntriesAreQueriedByPriceBetween_ThenNoNullGetsReturned(){
+        assertNotNull(service.findEntriesByPriceBetween(-100000, 100000));
+    }
+
+    @Test
+    void givenEntriesAreSaved_WhenEntriesAreQueriedByPriceBetween_ThenAllGetsReturned(){
+        IntStream.range(0, 5).forEach(i -> System.out.println(service.createRandomEntry()));
+        assertEquals(5, service.findEntriesByPriceBetween(-5000, 5000).size());
+    }
+
+
 
 }
