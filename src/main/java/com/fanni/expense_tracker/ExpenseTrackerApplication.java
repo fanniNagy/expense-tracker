6028920ca17cdffbf123fa14a3ee6c15fa38e03d
fanniNagy <nagy.fanni.5@gmail.com>
2021-02-12 17:04:47 +0100

    add logging using Slf4j

--- src/main/java/com/fanni/expense_tracker/ExpenseTrackerApplication.java
+++ src/main/java/com/fanni/expense_tracker/ExpenseTrackerApplication.java
@@ -1,9 +1,11 @@
 package com.fanni.expense_tracker;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class ExpenseTrackerApplication {
 
     public static void main(String[] args) {
