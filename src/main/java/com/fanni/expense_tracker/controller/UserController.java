d373f9520c7986450a5a3fc98087264243cfdeb3
fanniNagy <nagy.fanni.5@gmail.com>
2021-02-12 17:07:15 +0100

    add UserController

---
+++ src/main/java/com/fanni/expense_tracker/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.fanni.expense_tracker.controller;
+
+import com.fanni.expense_tracker.model.AppUser;
+import com.fanni.expense_tracker.service.UserRegistrationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserRegistrationService registrationService;
+
+    @Autowired
+    public UserController(UserRegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
+
+}
