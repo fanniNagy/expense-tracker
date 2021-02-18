0f118770f4a70f5cdd020a1a066ab20eef30f5a0
fanniNagy <nagy.fanni.5@gmail.com>
2021-02-18 14:48:42 +0100

    refactor: extract token building method for testing

--- src/main/java/com/fanni/expense_tracker/security/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/fanni/expense_tracker/security/jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -6,6 +6,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.crypto.SecretKey;
@@ -15,6 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.Date;
 
 public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -54,14 +56,30 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
                                             FilterChain chain,
                                             Authentication authResult) throws IOException, ServletException {
 
-        String token = Jwts.builder()
-                .setSubject(authResult.getName())
-                .claim("authorities", authResult.getAuthorities())
-                .setIssuedAt(new Date())
-                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusDays(jwtConfig.getTokenExpirationAfterDays())))
-                .signWith(secretKey)
-                .compact();
+        Date expiration = java.sql.Date.valueOf(LocalDate.now().plusDays(jwtConfig.getTokenExpirationAfterDays()));
+
+        String token = tokenBuilder(authResult.getName(),
+                authResult.getAuthorities(),
+                new Date(),
+                expiration,
+                secretKey);
 
         response.addHeader(jwtConfig.getAuthorizationHeader(), jwtConfig.getTokenPrefix() + token);
     }
+
+    public String tokenBuilder(String userName,
+                               Collection<? extends GrantedAuthority> authorities,
+                               Date issuedAt,
+                               Date expirationDate,
+                               SecretKey secretKey){
+        String token = Jwts.builder()
+                .setSubject(userName)
+                .claim("authorities", authorities)
+                .setIssuedAt(issuedAt)
+                .setExpiration(expirationDate)
+                .signWith(secretKey)
+                .compact();
+        return token;
+    }
+
 }
