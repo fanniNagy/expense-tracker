291408d4816a191a8b69a3421c3f6c7b0134cd1c
fanniNagy <nagy.fanni.5@gmail.com>
2021-02-18 10:39:43 +0100

    add UsernameAndPasswordAuthenticationRequest class for jwt auth

---
+++ src/main/java/com/fanni/expense_tracker/security/jwt/UsernameAndPasswordAuthenticationRequest.java
@@ -0,0 +1,15 @@
+package com.fanni.expense_tracker.security.jwt;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+public class UsernameAndPasswordAuthenticationRequest {
+
+    private String username;
+    private String password;
+
+}
