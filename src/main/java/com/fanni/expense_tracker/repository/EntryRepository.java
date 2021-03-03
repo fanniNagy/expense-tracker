9247b6f345728c65d5cca1df5bc08b1e83170e98
fanniNagy <nagy.fanni.5@gmail.com>
2021-03-03 11:17:53 +0100

    refactor inject AppUser dependency to repository

--- src/main/java/com/fanni/expense_tracker/repository/EntryRepository.java
+++ src/main/java/com/fanni/expense_tracker/repository/EntryRepository.java
@@ -6,6 +6,7 @@ import com.fanni.expense_tracker.model.Entry;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
@@ -16,30 +17,36 @@ public interface EntryRepository extends JpaRepository<Entry, Long> {
 
     Set<Entry> findAllEntryByUserId(Long userId);
 
-    Set<Entry> findEntriesByDateIsBetween(LocalDate from, LocalDate to);
+    @Query(nativeQuery = true,
+            value = "SELECT * " +
+                    "FROM Entry AS e " +
+                    "WHERE (e.date BETWEEN :fromDate AND :toDate)" +
+                    "AND (e.user_id = :userId) ")
+    Set<Entry> findEntriesOfUserByDateIsBetween(@Param("userId")Long userId, @Param("fromDate") LocalDate from, @Param("toDate") LocalDate to);
 
     @Query(nativeQuery = true,
             value = "SELECT * " +
                     "FROM Entry AS e " +
                     "WHERE (e.price BETWEEN :from AND :to) AND (e.user_id = :userId)" +
                     "GROUP BY e.user_id, e.id")
-    Set<Entry> findEntriesOfUserByPriceBetween(@Param("userId")Long userId, @Param("from")int from, @Param("to")int to);
+    Set<Entry> findEntriesOfUserByPriceBetween(@Param("userId") Long userId, @Param("from") int from, @Param("to") int to);
 
     @Transactional
     @Modifying
-    @Query("UPDATE Entry e SET e.category = ?2 where e.id = ?1")
-    void updateCategory(long id, Category category);
+    @Query("UPDATE Entry e SET e.category = ?2 WHERE (e.id = ?1) AND e.user.id=?3")
+    void updateCategory(Long id, Category category, Long userId);
+
+    @Query(value = "SELECT new com.fanni.expense_tracker.model.CategoryCount(e.category, SUM(e.price)) " +
+            "FROM Entry e " +
+            "JOIN AppUser u ON e.user.id = u.id " +
+            "GROUP BY  e.category " +
+            "ORDER BY SUM(e.price) ASC")
+    List<CategoryCount> getEntriesOfUserByCategories(@Param("userId") Long userId);
 
     @Query(value = "SELECT new com.fanni.expense_tracker.model.CategoryCount(e.category, SUM(e.price)) " +
             "FROM Entry AS e " +
+            "WHERE (e.price < 0) AND (e.user.id=:userId)" +
             "GROUP BY e.category " +
             "ORDER BY SUM(e.price) ASC")
-    List<CategoryCount> getEntriesByCategories();
-
-    @Query(value = "SELECT new com.fanni.expense_tracker.model.CategoryCount(e.category, SUM(e.price)) " +
-            "FROM Entry AS e " +
-            "WHERE e.price < 0" +
-            "GROUP BY e.category " +
-            "ORDER BY SUM(e.price) ASC")
-    List<CategoryCount> getSpendingByCategories();
+    List<CategoryCount> getSpendingOfUserByCategories(@Param("userId") Long userId);
 }
