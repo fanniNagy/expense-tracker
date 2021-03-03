c1f9910acc65f6e9f773bef38038bd6a1a008c16
fanniNagy <nagy.fanni.5@gmail.com>
2021-03-03 11:27:09 +0100

    add unit tests for random expense generation in EntryService

--- src/test/java/com/fanni/expense_tracker/service/EntryServiceUnitTest.java
+++ src/test/java/com/fanni/expense_tracker/service/EntryServiceUnitTest.java
@@ -4,10 +4,12 @@ import com.fanni.expense_tracker.model.AppUser;
 import com.fanni.expense_tracker.model.Category;
 import com.fanni.expense_tracker.model.Entry;
 import com.fanni.expense_tracker.repository.EntryRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDate;
@@ -39,13 +41,13 @@ public class EntryServiceUnitTest {
     }
 
     @Test
-    void givenAPositiveInteger_WhenRandomCategoryIsGenerated_AssertReturnsIncomeCategory(){
+    void givenAPositiveInteger_WhenRandomCategoryIsGenerated_AssertReturnsIncomeCategory() {
         assertTrue(Arrays.asList(Category.ONETIME_INCOME, Category.PAYMENT)
                 .contains(this.service.generateRandomCategoryToFitPrice(2)));
     }
 
     @Test
-    void givenANegativeInteger_WhenRandomCategoryIsGenerated_AssertReturnsExpenseCategory(){
+    void givenANegativeInteger_WhenRandomCategoryIsGenerated_AssertReturnsExpenseCategory() {
         assertFalse(Arrays.asList(Category.ONETIME_INCOME, Category.PAYMENT)
                 .contains(this.service.generateRandomCategoryToFitPrice(-2)));
     }
@@ -54,5 +56,14 @@ public class EntryServiceUnitTest {
     void givenRandomExpenseGenerated_WhenBuilt_ThenNameNotNull() {
         Entry generatedExpense = this.service.generateRandomExpense();
         assertNotNull(generatedExpense.getName());
+    }
+
+    @Test
+    void givenRandomExpenseCreated_WhenMethodCalled_AssertEntryCreatedWithCorrectUser() {
+        Entry randomExpense = this.service.createRandomExpense(user);
+        Mockito.verify(this.repository,
+                Mockito.times(1))
+                .saveAndFlush(Mockito.any(Entry.class));
+        Assertions.assertEquals(user, randomExpense.getUser());
     }
 }
