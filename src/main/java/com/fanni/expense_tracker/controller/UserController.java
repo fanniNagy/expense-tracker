0bc86294f4f5c2a33d090135d200ea8c233eea49
fanniNagy <nagy.fanni.5@gmail.com>
2021-03-08 16:56:18 +0100

    add endpoint to get user by username

--- src/main/java/com/fanni/expense_tracker/controller/UserController.java
+++ src/main/java/com/fanni/expense_tracker/controller/UserController.java
@@ -1,27 +1,32 @@
 package com.fanni.expense_tracker.controller;
 
 import com.fanni.expense_tracker.model.AppUser;
+import com.fanni.expense_tracker.service.AppUserService;
 import com.fanni.expense_tracker.service.UserRegistrationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
     private final UserRegistrationService registrationService;
+    private final AppUserService userService;
 
     @Autowired
-    public UserController(UserRegistrationService registrationService) {
+    public UserController(UserRegistrationService registrationService, AppUserService userService) {
+        this.userService = userService;
         this.registrationService = registrationService;
     }
 
     @PostMapping("/register")
     public AppUser registerNewUser(@RequestBody AppUser user){
         return registrationService.registerUser(user.getUsername(), user.getPassword());
+    }
+
+    @GetMapping("/{username}")
+    public AppUser getUserByUserName(@PathVariable String username){
+        return userService.getUserByUserName(username);
     }
 
 }
